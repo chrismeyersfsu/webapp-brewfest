ad2ade2ea767767ba9b60cf9bf1b5d41bd9c9e1f
Chris Meyers <chris.meyers.fsu@gmail.com>
2014-10-23 10:33:11 -0400

    added copy

--- www/js/main.js
+++ www/js/main.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
   var Store = window.localStorage;
+  var showPasteDialog = 'showPasteDialog';
 
+  ZeroClipboard.config( { swfPath: "http://cdnjs.cloudflare.com/ajax/libs/zeroclipboard/2.1.6/ZeroClipboard.swf" } );
+  var client = new ZeroClipboard($('#export'));
+
+/*
   var checkboxes = $('input');
   for (var i=0; i < checkboxes.length; ++i) {
     var checkbox = checkboxes[i];
@@ -18,7 +23,11 @@ $(document).ready(function() {
 
     $(checkbox).prop('checked', checked).checkboxradio('refresh');
   }
-
+  */
+  var beerList = getBeerList();
+  beerList.forEach(function(beer) {
+    $(beer.checkbox).prop('checked', beer.checked).checkboxradio('refresh');
+  });
 
 
   $('input').change(function() {
@@ -31,5 +40,52 @@ $(document).ready(function() {
       Store.setItem(beerName, "false");
     }
   });
+
+  $('#agitated').click(function() {
+    sessionStorage.setItem('showPasteDialog', false);
+  });
+
+  client.on('copy', function() {
+    console.log("Exported");
+    var beerList = getBeerList();
+    var beerNames = [];
+    beerList.forEach(function(beer) {
+      if (beer.checked) {
+        beerNames.push(beer.name);
+      }
+    });
+
+    //client.setText(beerNames);
+    client.setText(beerNames.toString());
+
+    //.popup( "option", "dismissible", true );
+    if (sessionStorage.getItem('showPasteDialog') !== "false") {
+      $('#pasteDialog').popup('open');
+    }
+  });
+
+  function getBeerList() {
+    var beerList = [];
+    var checkboxes = $('input');
+    for (var i=0; i < checkboxes.length; ++i) {
+      var checkbox = checkboxes[i];
+      var label = $('label[for="' + checkbox.id + '"]');
+      var beerName = $(label).html();
+
+      var checked = Store.getItem(beerName);
+      if (checked === undefined || checked === null || checked === "false") {
+        checked = false;
+      } else {
+        checked = true;
+      }
+      beerList.push({
+        checkbox: checkbox,
+        name: beerName,
+        checked: checked
+      });
+    }
+
+    return beerList;
+  }
 });
 
