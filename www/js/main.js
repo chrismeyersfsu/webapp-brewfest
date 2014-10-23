6e53ea1382a0cd99ef0c17d32941bc30b5f20367
Chris Meyers <chris.meyers.fsu@gmail.com>
2014-10-23 10:55:52 -0400

    got rid of zeroclipboard

--- www/js/main.js
+++ www/js/main.js
@@ -2,28 +2,6 @@ $(document).ready(function() {
   var Store = window.localStorage;
   var showPasteDialog = 'showPasteDialog';
 
-  ZeroClipboard.config( { swfPath: "http://cdnjs.cloudflare.com/ajax/libs/zeroclipboard/2.1.6/ZeroClipboard.swf" } );
-  var client = new ZeroClipboard($('#export'));
-
-/*
-  var checkboxes = $('input');
-  for (var i=0; i < checkboxes.length; ++i) {
-    var checkbox = checkboxes[i];
-    var label = $('label[for="' + checkbox.id + '"]');
-    var beerName = $(label).html();
-
-    var checked = Store.getItem(beerName);
-    if (checked === undefined || checked === null || checked === "false") {
-      checked = false;
-      console.log("UNCHECK ", beerName);
-    } else {
-      checked = true;
-      console.log("CHECK ", beerName);
-    }
-
-    $(checkbox).prop('checked', checked).checkboxradio('refresh');
-  }
-  */
   var beerList = getBeerList();
   beerList.forEach(function(beer) {
     $(beer.checkbox).prop('checked', beer.checked).checkboxradio('refresh');
@@ -41,11 +19,7 @@ $(document).ready(function() {
     }
   });
 
-  $('#agitated').click(function() {
-    sessionStorage.setItem('showPasteDialog', false);
-  });
-
-  client.on('copy', function() {
+  $('#export').click(function(e) {
     console.log("Exported");
     var beerList = getBeerList();
     var beerNames = [];
@@ -55,14 +29,22 @@ $(document).ready(function() {
       }
     });
 
-    //client.setText(beerNames);
-    client.setText(beerNames.toString());
-
-    //.popup( "option", "dismissible", true );
-    if (sessionStorage.getItem('showPasteDialog') !== "false") {
-      $('#pasteDialog').popup('open');
-    }
+    $('#pasteArea').val(beerNames);
+    $('#pasteDialog').popup('open');
   });
+/*
+  $('#pasteArea').focus(function() {
+      var $this = $(this);
+      $this.select();
+
+      // Work around Chrome's little problem
+      $this.mouseup(function() {
+          // Prevent further mouseup intervention
+          $this.unbind("mouseup");
+          return false;
+      });
+  });
+*/
 
   function getBeerList() {
     var beerList = [];
